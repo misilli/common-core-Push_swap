f4c979ab8e21760534473c7f4eee1796df479bf5
Azize Dursun <azizecakir16@gmail.com>
2026-09-20 05:58:26 +0300

    Add memory management functions for freeing resources
    Implement functions to free memory for stacks and arguments.

---
+++ free_utils.c
@@ -0,0 +1,45 @@
+void	free_stack(t_list **stack)
+{
+	t_list	*tmp;
+
+	if (!stack)
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
+
+void	free_args(char ***args)
+{
+	int	i;
+	int	j;
+
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		j = 0;
+		while (args[i][j])
+			free(args[i][j++]);
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+void	free_all(char ***args, t_main *data)
+{
+	if (data)
+	{
+		free_stack(&data->a);
+		free_stack(&data->b);
+		free(data->flags);
+		free(data->counts);
+		free(data);
+	}
+	free_args(args);
+}
