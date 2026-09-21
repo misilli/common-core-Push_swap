a5918df777594cc51c23c20d19ab6c46d6ae0ed4
Azize Dursun <azdursun@k1m06s01.42istanbul.com.tr>
2026-09-21 09:08:18 +0300

    add: free_utils.c and update bench.c header

---
+++ free_utils.c
@@ -0,0 +1,59 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                            :::      :::::::*/
+/*   free_utils.c                                           :+:      :+:    :+*/
+/*                                                        +:+ +:+         +:+ */
+/*   By: azdursun <azdursun@student.42istanbul.com.tr>  +#+  +:+       +#+    */
+/*                                                    +#+#+#+#+#+   +#+       */
+/*   Created: 2026/09/20 01:30:00 by azdursun               #+#    #+#        */
+/*   Updated: 2026/09/21 08:58:51 by azdursun              ###   ########.fr  */
+/*                                                                            */
+/******************************************************************************/
+
+#include "push_swap.h"
+
+void	free_stack(t_list **stack)
+{
+	t_list	*tmp;
+
+	if (!stack)
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
+
+void	free_args(char ***args)
+{
+	int	i;
+	int	j;
+
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		j = 0;
+		while (args[i][j])
+			free(args[i][j++]);
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+void	free_all(char ***args, t_main *data)
+{
+	if (data)
+	{
+		free_stack(&data->a);
+		free_stack(&data->b);
+		free(data->flags);
+		free(data->counts);
+		free(data);
+	}
+	free_args(args);
+}
